c7f87fe0e393df37d845c1cf9770ea76a82df3b4
castro2123 <ana.castro@ipvc.pt>
2026-01-20 03:04:06 +0000

    chunk1

--- processor/processing.py
+++ processor/processing.py
@@ -8,7 +8,7 @@ from config import PROCESSED_PATH
 
 async def process_chunk(chunk):
     chunk_mapped = map_dataframe(chunk)
-    financial_df = await enrich_chunk_fast(chunk_mapped, batch_size=50, batch_delay=0.05)
+    financial_df = await enrich_chunk(chunk_mapped, batch_size=50, batch_delay=0.05)
     chunk_enriched = pd.concat([chunk_mapped.reset_index(drop=True), financial_df], axis=1)
     return chunk_enriched
 
